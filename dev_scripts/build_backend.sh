203353038825913921238567c3368317b3819275
Kendrick Schellenberg <kendrick.m.schellenberg@gmail.com>
2025-02-11 14:46:55 -0700

    Dev scripts verify within virtual environment and repo root, ensures requirements installed

--- dev_scripts/build_backend.sh
+++ dev_scripts/build_backend.sh
@@ -11,17 +11,28 @@ export SECRET_KEY='django-insecure-0((h29a37al@^re@e!a#jclgqzdo2j!j&4t-!b8(-5#)=
 export DEBUG=1
 export DJANGO_ALLOWED_HOSTS="localhost 127.0.0.1 [::1] *"
 
+# Check if running inside a virtual environment
+if [[ "$VIRTUAL_ENV" != "" ]]; then
+    echo "Running inside a virtual environment"
+else
+    echo "Not running inside a virtual environment"
+    exit
+fi
+
 # Start the database container
 docker compose up -d db
 
 # Wait for the database to be ready
 while ! docker exec db pg_isready -U $DB_USER -d $DB_DATABASE; do
-  echo "Database still launching"
-  sleep 2
+    echo "Database still launching"
+    sleep 2
 done
 
 # Navigate to the backend directory
-cd backend
+cd backend || { echo "Failed to navigate to the backend directory"; echo "Make sure you're running from repo root"; exit 1; }
+
+# Install dependencies from requirements.txt
+pip install -r requirements.txt
 
 python3 manage.py makemigrations
 python3 manage.py migrate
--- dev_scripts/build_frontend.sh
+++ dev_scripts/build_frontend.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# Check if running inside a virtual environment
+if [[ "$VIRTUAL_ENV" != "" ]]; then
+    echo "Running inside a virtual environment"
+else
+    echo "Not running inside a virtual environment"
+    exit
+fi
+
 # Navigate to the frontend directory
-cd frontend
+cd frontend || { echo "Failed to navigate to the frontend directory"; echo "Make sure you're running from repo root"; exit 1; }
 
 # Install npm dependencies
 npm install
