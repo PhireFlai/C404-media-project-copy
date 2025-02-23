23c35f5d4c9fa3bc8219eb289f984c8d5faffcb9
Kendrick Schellenberg <kendrick.m.schellenberg@gmail.com>
2025-02-22 22:28:59 -0700

    Node management tests 5,11,12

--- backend/socialnetwork/views.py
+++ backend/socialnetwork/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import authenticate
 from django.core.files.base import ContentFile
-import requests
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
