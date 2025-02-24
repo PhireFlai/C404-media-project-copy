027362f0abc1fb7d49e96660a449b4a45de392e4
nandiniparekh <nandiniparekh@gmail.com>
2025-02-23 19:02:35 -0700

    Added test cases for Posts API

--- backend/socialnetwork/tests/test_post_api.py
+++ backend/socialnetwork/tests/test_post_api.py
@@ -25,42 +25,183 @@ class PostAPITestCase(APITestCase):
         self.token = Token.objects.get(user=self.user)
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
 
-    def test_list_posts(self):
-        """Test retrieving all posts."""
-        response = self.client.get('/api/posts/')
+    # def test_list_posts(self):
+    #     """Test retrieving all posts."""
+    #     response = self.client.get('/api/posts/')
+    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+    #     self.assertGreaterEqual(len(response.data), 2)
+    #     # Need to expand to check that the data is correct
+
+    # def test_retrieve_post(self):
+    #     """Test retrieving a single post."""
+    #     response = self.client.get(f'/api/posts/{self.post.id}/')
+    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+    #     self.assertEqual(response.data['title'], self.post.title)
+    #     # Need to expand to check that the data is correct
+
+
+    # def test_create_post(self):
+    #     """Test creating a new post."""
+    #     data = {"title": "New Post", "content": "New Content", "author": self.user.id}
+    #     response = self.client.post('/api/posts/', data)
+    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    #     self.assertEqual(response.data['title'], data['title'])
+    #     # Need to expand to check that the data is correct
+
+    # def test_update_post(self):
+    #     """Test updating a post."""
+    #     data = {"title": "Updated Title", "content": "Updated Content"}
+    #     response = self.client.put(f'/api/posts/{self.post.id}/', data)
+    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+    #     self.assertEqual(response.data['title'], data['title'])
+    #     # Need to expand to check that the data is correct
+
+    # def test_delete_post(self):
+    #     """Test deleting a post."""
+    #     response = self.client.delete(f'/api/posts/{self.post.id}/')
+    #     self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+    #     # Need to double check that the post is just updated in database with flag
+    
+    def test_public_posts_in_stream(self):
+        """Test that only public posts are fetched for the stream page."""
+        # Create a private post
+        private_post = Post.objects.create(
+            title="Private Post",
+            content="This is a private post",
+            author=self.user,
+            visibility=Post.FRIENDS_ONLY
+        )
+    
+        # Create an unlisted post
+        unlisted_post = Post.objects.create(
+            title="Unlisted Post",
+            content="This is an unlisted post",
+            author=self.user,
+            visibility=Post.UNLISTED
+        )
+    
+        # Create a deleted post
+        deleted_post = Post.objects.create(
+            title="Deleted Post",
+            content="This post is marked as deleted",
+            author=self.user,
+            visibility=Post.DELETED
+        )
+    
+        # Fetch all posts
+        response = self.client.get('/api/public-posts/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertGreaterEqual(len(response.data), 2)
-        # Need to expand to check that the data is correct
-
-    def test_retrieve_post(self):
-        """Test retrieving a single post."""
-        response = self.client.get(f'/api/posts/{self.post.id}/')
+    
+        # Check that all fetched posts are public
+        for post in response.data:
+            self.assertEqual(post['visibility'], Post.PUBLIC)
+    
+        # Ensure the private, unlisted, and deleted posts are not in the response
+        post_ids = [post['id'] for post in response.data]
+        self.assertNotIn(private_post.id, post_ids)
+        self.assertNotIn(unlisted_post.id, post_ids)
+        self.assertNotIn(deleted_post.id, post_ids)
+    
+    def test_posts_in_profile(self):
+        """Test that all posts (other than deleted) are fetched for the profile page."""
+        # Create a private post
+        private_post = Post.objects.create(
+            title="Private Post",
+            content="This is a private post",
+            author=self.user,
+            visibility=Post.FRIENDS_ONLY
+        )
+    
+        # Create an unlisted post
+        unlisted_post = Post.objects.create(
+            title="Unlisted Post",
+            content="This is an unlisted post",
+            author=self.user,
+            visibility=Post.UNLISTED
+        )
+    
+        # Create a public post
+        public_post = Post.objects.create(
+            title="Public Post",
+            content="This is a public post",
+            author=self.user,
+            visibility=Post.PUBLIC
+        )
+    
+        # Create a deleted post
+        deleted_post = Post.objects.create(
+            title="Deleted Post",
+            content="This post is marked as deleted",
+            author=self.user,
+            visibility=Post.DELETED
+        )
+    
+        # Fetch all posts for the user
+        response = self.client.get(f'/api/authors/{self.user.id}/posts/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['title'], self.post.title)
-        # Need to expand to check that the data is correct
+    
+        # Check that all fetched posts are not deleted
+        for post in response.data:
+            self.assertNotEqual(post['visibility'], Post.DELETED)
+    
+        # Ensure the deleted post is not in the response
+        post_ids = [str(post['id']) for post in response.data]
+        self.assertNotIn(str(deleted_post.id), post_ids)
+    
+        # Ensure the private, unlisted, and public posts are in the response
+        self.assertIn(str(private_post.id), post_ids)
+        self.assertIn(str(unlisted_post.id), post_ids)
+        self.assertIn(str(public_post.id), post_ids)
 
+    def test_author_can_delete_post(self):
+        """Test that an author can delete their own post."""
+        # Create two posts
+        post1 = Post.objects.create(
+            title="Post to be deleted",
+            content="This post will be deleted",
+            author=self.user,
+            visibility=Post.PUBLIC
+        )
+        post2 = Post.objects.create(
+            title="Post to remain",
+            content="This post will remain",
+            author=self.user,
+            visibility=Post.PUBLIC
+        )
 
-    def test_create_post(self):
-        """Test creating a new post."""
-        data = {"title": "New Post", "content": "New Content", "author": self.user.id}
-        response = self.client.post('/api/posts/', data)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(response.data['title'], data['title'])
-        # Need to expand to check that the data is correct
-
-    def test_update_post(self):
-        """Test updating a post."""
-        data = {"title": "Updated Title", "content": "Updated Content"}
-        response = self.client.put(f'/api/posts/{self.post.id}/', data)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['title'], data['title'])
-        # Need to expand to check that the data is correct
-
-    def test_delete_post(self):
-        """Test deleting a post."""
-        response = self.client.delete(f'/api/posts/{self.post.id}/')
+        # Delete the first post
+        response = self.client.delete(f'/api/authors/{self.user.id}/posts/{post1.id}/')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        # Need to double check that the post is just updated in database with flag
+
+        # Fetch all posts for the user
+        response = self.client.get(f'/api/authors/{self.user.id}/posts/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Check that the deleted post is not in the response
+        post_ids = [str(post['id']) for post in response.data]
+        self.assertNotIn(str(post1.id), post_ids)
+
+        # Ensure the non-deleted post is in the response
+        self.assertIn(str(post2.id), post_ids)
+    
+    def test_public_posts_visible_to_other_users(self):
+        """Test that public posts created by other users are visible to the current user."""
+        # Create a public post with other_user as the author
+        public_post = Post.objects.create(
+            title="Public Post by Other User",
+            content="This is a public post by another user",
+            author=self.other_user,
+            visibility=Post.PUBLIC
+        )
+
+        # Fetch all posts for the current user
+        response = self.client.get(f'/api/public-posts/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Check that the public post by other_user is in the response
+        post_ids = [str(post['id']) for post in response.data]
+        self.assertIn(str(public_post.id), post_ids)
+    
 
     # These don't pass, probably because the token isn't being setup/torn properly
     # def test_other_user_cannot_edit_post(self):
