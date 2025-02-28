c2df5a7d6c7f5c1d2abe977b6f31877459fa2030
nandiniparekh <nandiniparekh@gmail.com>
2025-02-27 18:00:05 -0700

    File Name Changes

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { logoutUser } from "../UserContext/userActions";
+import { logoutUser } from "../UserContext/userActions.js";
 import Dropdown from "./dropdown.js";
 import "./css/navbar.css";
 
