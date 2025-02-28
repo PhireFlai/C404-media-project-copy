6ed304763c755dca9d807bd5a68752dfaa4897d4
Andrew <azhang7@ualberta.ca>
2025-02-27 20:16:17 -0700

    fixed endpoints

--- backend/socialnetwork/views.py
+++ backend/socialnetwork/views.py
@@ -211,22 +211,6 @@ def updateUserProfile(request, userId):
         return Response({'message': 'Profile picture updated successfully'}, status=status.HTTP_200_OK)
     else:
         return Response({'error': 'No profile picture provided'}, status=status.HTTP_400_BAD_REQUEST)
-    
-# Creates a comment on a post
-'''@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def CreateComment(request, userId, pk):
-    post = get_object_or_404(Post, id=pk)
-    author = request.user
-    content = request.data.get('content')
-    data={'author': author.id, 'content': content, 'post': post.id}
-    serializer = CommentSerializer(data=data)
-    
-    if serializer.is_valid():
-        serializer.save()
-        requests.post(f'http://localhost:8000/api/authors/{userId}/inbox/', data=data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)'''
 
 # Posts a comment to an author's inbox
 @api_view(['POST'])
