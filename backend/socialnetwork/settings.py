b6f35a56c63b4de01585921c7abe6387b160a6eb
Kendrick Schellenberg <kendrick.m.schellenberg@gmail.com>
2025-03-18 06:26:22 +0000

    Remote backend access semi-functional

--- backend/socialnetwork/settings.py
+++ backend/socialnetwork/settings.py
@@ -86,6 +86,8 @@ WSGI_APPLICATION = 'socialnetwork.wsgi.application'
 
 # Allow React App to connect to the backend
 CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
+# settings.py
+SECURE_CROSS_ORIGIN_OPENER_POLICY = None # Switch back TODO
 
 
 # Database
